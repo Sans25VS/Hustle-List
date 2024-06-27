21067123a2d494722bd5ecb50332cd9d391f902f
sanskar <sanskargupta37081@gmail.com>
2024-06-27 21:42:17 +0530

    Backend added Convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>HUSTLE LIST</h1>
+      <Button>STEP 1</Button>
     </main>
   );
 }
